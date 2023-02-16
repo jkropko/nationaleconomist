d89639e63c650c41262e26223ea473f71dd0bdd7
Mike Wetklow <mw8hu@virginia.edu>
2023-02-15 20:49:25 -0500

    first script run Feb 15!

---
+++ mike_sandbox/pdfparsescript_msw.sh
@@ -0,0 +1,56 @@
+python pdfparse.py mar1889.pdf -o mar1889text.txt
+python pdfparse.py apr1889.pdf -o apr1889text.txt
+python pdfparse.py may1889.pdf -o may1889text.txt
+python pdfparse.py june1889.pdf -o june1889text.txt
+python pdfparse.py july1889.pdf -o july1889text.txt
+python pdfparse.py aug1889.pdf -o aug1889text.txt
+python pdfparse.py sept1889.pdf -o sept1889text.txt
+python pdfparse.py oct1889.pdf -o oct1889text.txt
+python pdfparse.py nov1889.pdf -o nov1889text.txt
+python pdfparse.py dec1889.pdf -o dec1889text.txt
+python pdfparse.py jan1890.pdf -o july1890text.txt
+python pdfparse.py feb1890.pdf -o feb1890text.txt
+python pdfparse.py mar1890.pdf -o mar1890text.txt
+python pdfparse.py apr1890.pdf -o apr1890text.txt
+python pdfparse.py may1890.pdf -o may1890text.txt
+python pdfparse.py june1890.pdf -o june1890text.txt
+python pdfparse.py july1890.pdf -o july1890text.txt
+python pdfparse.py aug1890.pdf -o aug1890text.txt
+
+python pdfparse.py apr1892.pdf -o apr1892text.txt
+python pdfparse.py aug1892.pdf -o aug1892text.txt
+python pdfparse.py dec1892.pdf -o dec1892text.txt
+python pdfparse.py dec1891.pdf -o dec1891text.txt
+python pdfparse.py feb1892.pdf -o feb1892text.txt
+python pdfparse.py feb1893.pdf -o feb1893text.txt
+python pdfparse.py jan1892.pdf -o jan1892text.txt
+python pdfparse.py jan1893.pdf -o jan1893text.txt
+python pdfparse.py july1892.pdf -o july1892text.txt
+python pdfparse.py july1893.pdf -o july1893text.txt
+python pdfparse.py june1892.pdf -o june1892text.txt
+python pdfparse.py mar1892.pdf -o mar1892text.txt
+python pdfparse.py mar1893.pdf -o mar1893text.txt
+python pdfparse.py may1892.pdf -o may1892text.txt
+python pdfparse.py nov1892.pdf -o nov1892text.txt
+python pdfparse.py nov1891.pdf -o nov1891text.txt
+python pdfparse.py oct1892.pdf -o oct1892text.txt
+python pdfparse.py oct1891.pdf -o oct1891text.txt
+python pdfparse.py sept1892.pdf -o sept1892text.txt
+
+python pdfparse.py dds-45020.pdf -o sept1890text.txt
+python pdfparse.py dds-45022.pdf -o supplementtext.txt
+python pdfparse.py dds-45023.pdf -o almanac1890text.txt
+
+python pdfparse.py dds-45029.pdf -o sept1890text.txt
+python pdfparse.py dds-45030.pdf -o dec1890text.txt
+python pdfparse.py dds-45036.pdf -o jan1891text.txt
+python pdfparse.py dds-45037.pdf -o feb1891text.txt
+python pdfparse.py dds-45038.pdf -o mar1891text.txt
+
+python pdfparse.py dds-45039.pdf -o april1891text.txt
+python pdfparse.py dds-45040.pdf -o may1891text.txt
+python pdfparse.py dds-45041.pdf -o june1891text.txt
+python pdfparse.py dds-45042.pdf -o july1891text.txt
+python pdfparse.py dds-45043.pdf -o aug1891text.txt
+python pdfparse.py dds-45044.pdf -o sept1891text.txt
+python pdfparse.py dds-45045.pdf -o oct1891text.txt
